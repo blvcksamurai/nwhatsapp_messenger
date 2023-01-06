363ad46c6c48768c931c998cf80b98d71af045d6
blvcksamurai <theblvcksamurai@gmail.com>
2023-01-06 19:28:36 +0100

    Virtualisaton.dart

--- lib/features/welcome/pages/virtualisation.dart
+++ lib/features/welcome/pages/virtualisation.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
+import '../../../common/utils/colors.dart';
+
+class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Icon(
+        Icons.language,
+        color: Coloors.greenDark,
+      ),
+    );
   }
 }
